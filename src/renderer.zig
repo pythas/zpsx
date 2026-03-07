17175e226ae1e2ba04f9d4556a49e517620e870c
Johan Gronberg <pythas@gmail.com>
2026-03-07 10:33:47 +0100

    winding order agnostic edge test

--- src/renderer.zig
+++ src/renderer.zig
@@ -35,31 +35,6 @@ pub const Renderer = struct {
         _ = self;
     }
 
-    // pub fn pushShadedTriangle(
-    //     self: *Self,
-    //     p1: Point,
-    //     c1: Color,
-    //     p2: Point,
-    //     c2: Color,
-    //     p3: Point,
-    //     c3: Color,
-    // ) void {
-    //     if (self.vertex_count + 3 > self.vertex_buffer.len) return;
-    //
-    //     const fx1 = (@as(f32, @floatFromInt(p1.x)) / 512.0) - 1.0;
-    //     const fy1 = 1.0 - (@as(f32, @floatFromInt(p1.y)) / 256.0);
-    //     const fx2 = (@as(f32, @floatFromInt(p2.x)) / 512.0) - 1.0;
-    //     const fy2 = 1.0 - (@as(f32, @floatFromInt(p2.y)) / 256.0);
-    //     const fx3 = (@as(f32, @floatFromInt(p3.x)) / 512.0) - 1.0;
-    //     const fy3 = 1.0 - (@as(f32, @floatFromInt(p3.y)) / 256.0);
-    //
-    //     self.vertex_buffer[self.vertex_count] = .{ .x = fx1, .y = fy1, .r = c1.r, .g = c1.g, .b = c1.b, .a = 255 };
-    //     self.vertex_buffer[self.vertex_count + 1] = .{ .x = fx2, .y = fy2, .r = c2.r, .g = c2.g, .b = c2.b, .a = 255 };
-    //     self.vertex_buffer[self.vertex_count + 2] = .{ .x = fx3, .y = fy3, .r = c3.r, .g = c3.g, .b = c3.b, .a = 255 };
-    //
-    //     self.vertex_count += 3;
-    // }
-
     pub fn pushShadedTriangle(
         self: *Self,
         gpu: *Gpu,
@@ -71,11 +46,16 @@ pub const Renderer = struct {
         c3: Color,
     ) void {
         _ = self;
-        // _ = c1;
-        _ = c2;
-        _ = c3;
 
-        var bbx = Bbx.fromPoints(&.{ p1, p2, p3 });
+        var points = [3]Point{ p1, p2, p3 };
+        var colors = [3]Color{ c1, c2, c3 };
+
+        if (edgeFunction(points[0], points[1], points[2]) < 0) {
+            std.mem.swap(Point, &points[1], &points[2]);
+            std.mem.swap(Color, &colors[1], &colors[2]);
+        }
+
+        var bbx = Bbx.fromPoints(&points);
 
         bbx.min_x = @max(bbx.min_x, @as(i16, @intCast(gpu.drawing_area_left)));
         bbx.max_x = @min(bbx.max_x, @as(i16, @intCast(gpu.drawing_area_right)));
@@ -90,12 +70,18 @@ pub const Renderer = struct {
         while (y <= bbx.max_y) : (y += 1) {
             var x: i32 = bbx.min_x;
             while (x <= bbx.max_x) : (x += 1) {
-                var inside = true;
-                inside &= edgeFunction(p1, p2, x, y);
-                inside &= edgeFunction(p2, p3, x, y);
-                inside &= edgeFunction(p3, p1, x, y);
+                const p = Point{
+                    .x = @intCast(x),
+                    .y = @intCast(y),
+                };
 
-                if (inside) {
+                const w0 = edgeFunction(points[1], points[2], p);
+                const w1 = edgeFunction(points[2], points[0], p);
+                const w2 = edgeFunction(points[0], points[1], p);
+
+                const is_inside = w0 >= 0 and w1 >= 0 and w2 >= 0;
+
+                if (is_inside) {
                     gpu.putPixel(@as(i16, @intCast(x)), @as(i16, @intCast(y)), c1.r, c1.g, c1.b);
                 }
             }
@@ -128,11 +114,13 @@ const Bbx = struct {
     }
 };
 
-fn edgeFunction(a: Point, b: Point, cx: i32, cy: i32) bool {
+fn edgeFunction(a: Point, b: Point, c: Point) i32 {
     const ax: i32 = a.x;
     const ay: i32 = a.y;
     const bx: i32 = b.x;
     const by: i32 = b.y;
+    const cx: i32 = c.x;
+    const cy: i32 = c.y;
 
-    return ((cx - ax) * (by - ay) - (cy - ay) * (bx - ax) >= 0);
+    return (cx - ax) * (by - ay) - (cy - ay) * (bx - ax);
 }
